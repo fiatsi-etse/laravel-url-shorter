52a26160a9ba3045c257d67ee76581a8d16aa257
EFIATSI <etse.fiatsi@digital.gouv.tg>
2024-11-14 20:08:52 +0000

    [UPDATE] Ressource route instead of personnal one

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
-    Route::get('/url', [UrlController::class, 'index'])->name('url.list');
-    Route::get('/url/create', [UrlController::class, 'create'])->name('url.create');
-    Route::post('/url', [UrlController::class, 'store'])->name('url.store');
-    Route::put('/url', [UrlController::class, 'update'])->name('url.update');
-    Route::delete('/url', [UrlController::class, 'destroy'])->name('url.destroy');
+    Route::resource('url', UrlController::class)->names([
+        'store' => 'url.store',
+        'index' => 'url.list',
+        'update' => 'url.update'
+    ]);
+    // Route::get('/url', [UrlController::class, 'index'])->name('url.list');
+    // Route::get('/url/create', [UrlController::class, 'create'])->name('url.create');
+    // Route::post('/url', [UrlController::class, 'store'])->name('url.store');
+    // Route::put('/url', [UrlController::class, 'update'])->name('url.update');
+    // Route::delete('/url', [UrlController::class, 'destroy'])->name('url.destroy');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
